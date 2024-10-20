0b724a653ea32d26a8fd888db841283144c527e5
Ositha Dewruwan <71217790+Dewruwan95@users.noreply.github.com>
2024-10-20 14:11:44 +0530

    created basic header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <div>
+      <header className="bg-purple-600 w-full h-[120px] flex items-center px-[150px] shadow-xl">
+        <h1 className="text-[60px] text-purple-50 ">Hotel ABC</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
