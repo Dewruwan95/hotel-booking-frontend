873afe00f516f69919cdc37f9ee5b12bfed47114
Ositha Dewruwan <71217790+Dewruwan95@users.noreply.github.com>
2024-10-21 23:12:20 +0530

    created complete admin navigation menu

--- src/components/pages/adminPages/dashboard/NavigationItem.jsx
+++ src/components/pages/adminPages/dashboard/NavigationItem.jsx
@@ -3,14 +3,19 @@ import { Link } from "react-router-dom";
 function NavigationItem(props) {
   return (
     <div
-      className="w-[90%] text-white text-[30px] hover:bg-purple-800 flex items-center border-y-4 border-r-4 border-purple-950 rounded-r-[50px]
-
-
-
-"
+      className="
+      w-[90%] 
+      my-[5px]
+      py-[5px] pl-[25px] 
+      bg-purple-300 hover:bg-purple-900 
+      font-semibold text-[30px] text-purple-900 hover:text-white
+      flex items-center 
+      border-y-4 border-r-4 rounded-r-[50px] border-purple-950 
+      shadow-md hover:shadow-lg
+      transition duration-300 ease-in-out"
     >
-      <props.itemIcon />
-      <Link to="admin/booking">{props.itemName}</Link>
+      <props.itemIcon className="mr-[10px]" />
+      <Link to={props.itemLink}>{props.itemName}</Link>
     </div>
   );
 }
--- src/components/pages/adminPages/dashboard/NavigationItemList.jsx
+++ src/components/pages/adminPages/dashboard/NavigationItemList.jsx
@@ -1,9 +1,54 @@
+import { BsBookmarkCheckFill, BsFillDoorOpenFill } from "react-icons/bs";
 import NavigationItem from "./NavigationItem";
 import { RiDashboardHorizontalFill } from "react-icons/ri";
+import { MdCategory } from "react-icons/md";
+import { ImUsers } from "react-icons/im";
+import { IoChatboxEllipses } from "react-icons/io5";
+import { PiListStarFill } from "react-icons/pi";
+import { IoIosImage } from "react-icons/io";
 function NavigationItemList() {
   return (
     <div>
-      <NavigationItem itemName="Booking" itemIcon={RiDashboardHorizontalFill} />
+      <NavigationItem
+        itemName="Dashboard"
+        itemIcon={RiDashboardHorizontalFill}
+        itemLink="admin/dashboard"
+      />
+      <NavigationItem
+        itemName="Bookings"
+        itemIcon={BsBookmarkCheckFill}
+        itemLink="admin/booking"
+      />
+      <NavigationItem
+        itemName="Rooms"
+        itemIcon={BsFillDoorOpenFill}
+        itemLink="admin/rooms"
+      />
+      <NavigationItem
+        itemName="Categories"
+        itemIcon={MdCategory}
+        itemLink="admin/categories"
+      />
+      <NavigationItem
+        itemName="Users"
+        itemIcon={ImUsers}
+        itemLink="admin/users"
+      />
+      <NavigationItem
+        itemName="Feedbacks"
+        itemIcon={PiListStarFill}
+        itemLink="admin/feedback"
+      />
+      <NavigationItem
+        itemName="Ticketing"
+        itemIcon={IoChatboxEllipses}
+        itemLink="admin/ticketing"
+      />
+      <NavigationItem
+        itemName="Gallery"
+        itemIcon={IoIosImage}
+        itemLink="admin/gallery"
+      />
     </div>
   );
 }
