bcb0eef1f38773b7c8dcb220f2639a384682ec8c
Ositha Dewruwan <71217790+Dewruwan95@users.noreply.github.com>
2024-10-23 06:53:06 +0530

    created admin rooms data

--- src/pages/adminPages/rooms/AdminRooms.jsx
+++ src/pages/adminPages/rooms/AdminRooms.jsx
@@ -1,8 +1,30 @@
+import DataTable from "../../../components/adminDataTable/DataTable";
+import { rooms } from "../../../data/RoomsData";
 function AdminRooms() {
+  // Column headers for Room table
+  const roomColumns = [
+    "Room No",
+    "Category",
+    "Max Guests",
+    "Special Desc",
+    "Notes",
+    "Availability",
+    "Action",
+  ];
+
+  // Fields corresponding to the columns
+  const roomFields = [
+    "roomNo",
+    "category",
+    "maxGuests",
+    "specialDescription",
+    "notes",
+    "available",
+  ];
   return (
-    <div>
-      <h1>Rooms</h1>
-    </div>
+    <>
+      <DataTable columns={roomColumns} fields={roomFields} data={rooms} />
+    </>
   );
 }
 
