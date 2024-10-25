f32a7c86b2e7f08409bea10ad4bfede1fcb17e6e
Ositha Dewruwan <71217790+Dewruwan95@users.noreply.github.com>
2024-10-25 22:27:49 +0530

    created seperate user login component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/clientPages/home/HomePage";
 import AdminDashboard from "./pages/adminPages/dashboard/AdminDashboard";
 import LoginPage from "./pages/loginPage/LoginPage";
+import RegisterPage from "./pages/registerPage/RegisterPage";
+import UserLogin from "./components/userLoginAndRegister/UserLogin";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Routes path="/*">
           <Route path="/admin/*" element={<AdminDashboard />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/login2" element={<UserLogin />} />
+          <Route path="/register" element={<RegisterPage />} />
           <Route path="/*" element={<HomePage />} />
         </Routes>
       </BrowserRouter>
---
+++ src/components/userLoginAndRegister/UserLogin.jsx
@@ -0,0 +1,102 @@
+import axios from "axios";
+import { useState } from "react";
+import { FaRegUser } from "react-icons/fa";
+import { GoKey } from "react-icons/go";
+import { IoMdLogIn } from "react-icons/io";
+
+function UserLogin() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState(false);
+
+  function handleLogin() {
+    axios
+      .post(import.meta.env.VITE_BACKEND_URL + "/api/users/login", {
+        email: email,
+        password: password,
+      })
+      .then((res) => {
+        localStorage.setItem("token", res.data.token);
+        if (res.data.user) {
+          if (res.data.user.type === "admin") {
+            window.location.href = "/admin";
+          } else {
+            window.location.href = "/";
+          }
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  return (
+    <>
+      <div className="bg-white w-full h-[100vh] flex items-center justify-center">
+        <div className="  flex flex-col   ">
+          {/* email field */}
+          <div className="flex my-4">
+            <div className=" bg-purple-300 text-purple-500 h-[45px] w-[45px] flex items-center justify-center rounded-l-[6px]">
+              <FaRegUser className="h-4 w-4" />
+            </div>
+
+            <input
+              type="email"
+              placeholder="Email"
+              className="w-[360px] h-[45px] px-[10px] py-[5px] rounded-r-[6px] border-[1px] border-gray-400 focus:border-[2px] focus:border-purple-400 focus:outline-none"
+              defaultValue={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          {/* password field */}
+          <div className="flex my-4">
+            <div className=" bg-purple-300 text-purple-500 h-[45px] w-[45px] flex items-center justify-center rounded-l-[6px]">
+              <GoKey className="h-4 w-4" />
+            </div>
+
+            <input
+              type="password"
+              placeholder="Password"
+              className="w-[360px] h-[45px] px-[10px] py-[5px] rounded-r-[6px] border-[1px] border-gray-400 focus:border-[2px] focus:border-purple-400 focus:outline-none"
+              defaultValue={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          {/* Remember me & Forgot Password */}
+          <div className="my-4 px-8 flex justify-between w-full">
+            <span className=" text-gray-600">
+              <input
+                type="checkbox"
+                className="mr-2"
+                defaultValue={rememberMe}
+                onChange={(e) => setRememberMe(e.target.checked)}
+              />
+              Remember me
+            </span>
+
+            <a href="#" className=" text-purple-500 hover:underline">
+              Forgot Password?
+            </a>
+          </div>
+
+          {/* button */}
+          <div className="my-4">
+            <button
+              className="w-[405px] h-[40px] bg-purple-600 text-white text-lg 
+                      font-semibold rounded-lg shadow-md hover:bg-purple-800 hover:shadow-lg 
+                      transition duration-300 ease-in-out flex items-center justify-center"
+              onClick={handleLogin}
+            >
+              <IoMdLogIn className="mr-2" />
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default UserLogin;
