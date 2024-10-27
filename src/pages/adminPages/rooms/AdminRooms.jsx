ebbe23a1f37048244da564e52a4a3732181eee3f
Ositha Dewruwan <71217790+Dewruwan95@users.noreply.github.com>
2024-10-27 22:51:22 +0530

    created get rooms data from backend and delete room functionality

--- src/pages/adminPages/rooms/AdminRooms.jsx
+++ src/pages/adminPages/rooms/AdminRooms.jsx
@@ -1,6 +1,44 @@
+import { useEffect, useState } from "react";
 import DataTable from "../../../components/adminDashboard/adminDataTable/DataTable";
-import { rooms } from "../../../data/RoomsData";
+import axios from "axios";
+
 function AdminRooms() {
+  const [roomsData, setRoomsData] = useState([]);
+  const [isRoomsDataLoaded, setIsRoomsDataLoaded] = useState(false);
+
+  useEffect(() => {
+    if (!isRoomsDataLoaded) {
+      //get rooms data from backend
+      axios
+        .get(import.meta.env.VITE_BACKEND_URL + "/api/rooms")
+        .then((res) => {
+          setRoomsData(res.data.rooms);
+          setIsRoomsDataLoaded(true);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [isRoomsDataLoaded]);
+
+  // room delete function
+  function handleDelete(roomNo) {
+    if (window.confirm(`Are you sure you want to delete Room ${roomNo}?`)) {
+      const token = localStorage.getItem("token");
+      axios
+        .delete(`${import.meta.env.VITE_BACKEND_URL}/api/rooms/${roomNo}`, {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        .then(() => {
+          setIsRoomsDataLoaded(false);
+        })
+        .catch((error) => {
+          console.error("Failed to delete room:", error);
+          alert("Failed to delete room. Please try again.");
+        });
+    }
+  }
+
   // Column headers for Room table
   const roomColumns = [
     "Room No",
@@ -23,7 +61,13 @@ function AdminRooms() {
   ];
   return (
     <>
-      <DataTable columns={roomColumns} fields={roomFields} data={rooms} />
+      <DataTable
+        columns={roomColumns}
+        fields={roomFields}
+        data={roomsData}
+        deleteElement={handleDelete}
+        elementIdentifier={"roomNo"}
+      />
     </>
   );
 }
