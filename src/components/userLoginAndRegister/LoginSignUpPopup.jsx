eb7ba3894d37fd2d4f623c1d324ab34248a687d2
Ositha Dewruwan <71217790+Dewruwan95@users.noreply.github.com>
2024-10-31 21:27:19 +0530

    created get feedback data from backend and delete feedback functionality

--- src/components/userLoginAndRegister/LoginSignUpPopup.jsx
+++ src/components/userLoginAndRegister/LoginSignUpPopup.jsx
@@ -9,7 +9,7 @@ function LoginSignUpPopup({ onClose, onLogin }) {
   return (
     <>
       {/*  background */}
-      <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50 z-50">
+      <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-80 z-50">
         {/* component background */}
         <div className="w-[950px] h-[650px] bg-purple-50 rounded-lg relative">
           {/* close button */}
--- src/pages/adminPages/feedbacks/AdminFeedback.jsx
+++ src/pages/adminPages/feedbacks/AdminFeedback.jsx
@@ -1,7 +1,57 @@
+import { useEffect, useState } from "react";
 import DataTable from "../../../components/adminDashboard/adminDataTable/DataTable";
-import { feedback } from "../../../data/FeedbackData";
+import axios from "axios";
 
 function AdminFeedback() {
+  const [feedbackData, setFeedbackData] = useState([]);
+  const [isFeedbacksLoaded, setIsFeedbacksLoaded] = useState(false);
+
+  useEffect(() => {
+    if (!isFeedbacksLoaded) {
+      fetchFeedbackData();
+    }
+  }, [isFeedbacksLoaded]);
+
+  // fetch feedback data function
+  async function fetchFeedbackData() {
+    try {
+      const token = localStorage.getItem("token");
+      const res = await axios.get(
+        import.meta.env.VITE_BACKEND_URL + "/api/feedbacks",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+
+      setFeedbackData(res.data.feedbacks);
+      setIsFeedbacksLoaded(true);
+    } catch (error) {
+      console.error("Failed to fetch feedbacks:", error);
+    }
+  }
+
+  // feedback delete function
+  async function handleDelete(feedbackId) {
+    const confirmDelete = window.confirm(
+      `Are you sure you want to delete Feedback ${feedbackId}?`
+    );
+    if (confirmDelete) {
+      const token = localStorage.getItem("token");
+      try {
+        await axios.delete(
+          import.meta.env.VITE_BACKEND_URL + `/api/feedbacks/${feedbackId}`,
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
+        setIsFeedbacksLoaded(false);
+      } catch (error) {
+        console.error("Failed to delete feedback:", error);
+        alert("Failed to delete feedback. Please try again.");
+      }
+    }
+  }
+
   // Column headers for Room table
   const feedbackColumns = [
     "Date",
@@ -29,7 +79,9 @@ function AdminFeedback() {
       <DataTable
         columns={feedbackColumns}
         fields={feedbackFields}
-        data={feedback}
+        data={feedbackData}
+        deleteElement={handleDelete}
+        elementIdentifier="feedbackId"
       />
     </>
   );
