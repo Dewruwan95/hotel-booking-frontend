e128835ac413961ab33a061403398e291d4bfda0
Ositha Dewruwan <71217790+Dewruwan95@users.noreply.github.com>
2024-11-02 06:13:04 +0530

    created add new element button in data summary component

--- src/components/adminDashboard/adminDataSummary/AdminDataSummary.jsx
+++ src/components/adminDashboard/adminDataSummary/AdminDataSummary.jsx
@@ -1,8 +1,15 @@
+import { FaPlus } from "react-icons/fa";
+
 function AdminDataSummary() {
   return (
     <div>
-      <div className="w-full h-[200px] rounded-tl-[10px] px-[20px] pt-[20px]">
-        <div className="w-full h-full rounded-[10px] bg-purple-600"></div>
+      <div className="w-full h-[150px] rounded-tl-[10px] px-[20px] pt-[20px] flex items-center justify-center">
+        <div className="w-[100%] h-full rounded-[10px] bg-purple-600"></div>
+        <div className="w-[130px] h-full rounded-[10px] ml-[20px]">
+          <button className="h-[130px] w-[130px] rounded-full  bg-purple-600 flex justify-center items-center hover:bg-purple-500">
+            <FaPlus className="text-white text-[70px] transition-transform duration-300 ease-in-out hover:rotate-90" />
+          </button>
+        </div>
       </div>
     </div>
   );
