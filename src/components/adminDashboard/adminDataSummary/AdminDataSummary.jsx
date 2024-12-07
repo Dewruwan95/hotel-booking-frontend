cd89247df33b06f6c90f003a8d04af2ca4da45e2
Ositha Dewruwan <71217790+Dewruwan95@users.noreply.github.com>
2024-12-07 08:09:27 +0530

    created admin categories data summary

--- src/components/adminDashboard/adminDataSummary/AdminDataSummary.jsx
+++ src/components/adminDashboard/adminDataSummary/AdminDataSummary.jsx
@@ -1,50 +1,70 @@
 import { FaPlus } from "react-icons/fa";
 
-function AdminDataSummary({ onAddElementClick, roomsSummary }) {
-  console.log(roomsSummary);
-
+function AdminDataSummary({
+  onAddElementClick,
+  roomsSummary,
+  categoriesSummary,
+}) {
   return (
     <div>
       <div className="w-full h-[150px] rounded-tl-[10px] px-[20px] pt-[20px] flex items-center justify-center">
         {/* summary area */}
         <div className="w-[100%] h-full rounded-[10px] bg-purple-600 flex flex-row justify-center items-center">
-          <div className="flex h-full justify-between w-full px-[20px]">
-            <div className="flex h-full items-center justify-center py-[10px]">
-              <div className="bg-white px-[20px] h-full  rounded-[10px] flex items-center text-purple-800">
-                <span className="text-[30px] text-center">
-                  Total <br /> Rooms
-                </span>
-                <span className="text-[30px] text-center ml-2"> : </span>
-                <span className="ml-[10px] text-[50px] font-extrabold text-center">
-                  {roomsSummary.totalRooms}
-                </span>
+          {roomsSummary ? (
+            <div className="flex h-full justify-between w-full px-[20px]">
+              <div className="flex h-full items-center justify-center py-[10px]">
+                <div className="bg-white px-[20px] h-full  rounded-[10px] flex items-center text-purple-800">
+                  <span className="text-[30px] text-center">
+                    Total <br /> Rooms
+                  </span>
+                  <span className="text-[30px] text-center ml-2"> : </span>
+                  <span className="ml-[10px] text-[50px] font-extrabold text-center">
+                    {roomsSummary.totalRooms}
+                  </span>
+                </div>
               </div>
-            </div>
 
-            <div className="flex h-full items-center justify-center py-[10px]">
-              <div className="bg-white px-[20px] h-full  rounded-[10px] flex items-center text-purple-800">
-                <span className="text-[30px] text-center">
-                  Available <br /> Rooms
-                </span>
-                <span className="text-[30px] text-center ml-2"> : </span>
-                <span className="ml-[10px] text-[50px] font-extrabold text-center">
-                  {roomsSummary.availableRooms}
-                </span>
+              <div className="flex h-full items-center justify-center py-[10px]">
+                <div className="bg-white px-[20px] h-full  rounded-[10px] flex items-center text-purple-800">
+                  <span className="text-[30px] text-center">
+                    Available <br /> Rooms
+                  </span>
+                  <span className="text-[30px] text-center ml-2"> : </span>
+                  <span className="ml-[10px] text-[50px] font-extrabold text-center">
+                    {roomsSummary.availableRooms}
+                  </span>
+                </div>
               </div>
-            </div>
 
-            <div className="flex h-full items-center justify-center py-[10px]">
-              <div className="bg-white px-[20px] h-full  rounded-[10px] flex items-center text-purple-800">
-                <span className="text-[30px] text-center">
-                  Unavailable <br /> Rooms
-                </span>
-                <span className="text-[30px] text-center ml-2"> : </span>
-                <span className="ml-[10px] text-[50px] font-extrabold text-center">
-                  {roomsSummary.notAvailableRooms}
-                </span>
+              <div className="flex h-full items-center justify-center py-[10px]">
+                <div className="bg-white px-[20px] h-full  rounded-[10px] flex items-center text-purple-800">
+                  <span className="text-[30px] text-center">
+                    Unavailable <br /> Rooms
+                  </span>
+                  <span className="text-[30px] text-center ml-2"> : </span>
+                  <span className="ml-[10px] text-[50px] font-extrabold text-center">
+                    {roomsSummary.notAvailableRooms}
+                  </span>
+                </div>
               </div>
             </div>
-          </div>
+          ) : categoriesSummary ? (
+            <div className="flex h-full justify-between w-full px-[20px]">
+              <div className="flex h-full items-center justify-center py-[10px]">
+                <div className="bg-white px-[20px] h-full  rounded-[10px] flex items-center text-purple-800">
+                  <span className="text-[30px] text-center">
+                    Total <br /> Categories
+                  </span>
+                  <span className="text-[30px] text-center ml-2"> : </span>
+                  <span className="ml-[10px] text-[50px] font-extrabold text-center">
+                    {categoriesSummary.totalCategories}
+                  </span>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div></div>
+          )}
         </div>
 
         {/* button area */}
