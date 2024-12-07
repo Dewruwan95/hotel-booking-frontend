d4894efc391e072e4fac155376ff697e5353a9a1
Ositha Dewruwan <71217790+Dewruwan95@users.noreply.github.com>
2024-12-07 08:30:55 +0530

    created admin users data summary

--- src/components/adminDashboard/adminDataSummary/AdminDataSummary.jsx
+++ src/components/adminDashboard/adminDataSummary/AdminDataSummary.jsx
@@ -4,6 +4,7 @@ function AdminDataSummary({
   onAddElementClick,
   roomsSummary,
   categoriesSummary,
+  usersSummary,
 }) {
   return (
     <div>
@@ -11,6 +12,7 @@ function AdminDataSummary({
         {/* summary area */}
         <div className="w-[100%] h-full rounded-[10px] bg-purple-600 flex flex-row justify-center items-center">
           {roomsSummary ? (
+            // rooms summary section
             <div className="flex h-full justify-between w-full px-[20px]">
               <div className="flex h-full items-center justify-center py-[10px]">
                 <div className="bg-white px-[20px] h-full  rounded-[10px] flex items-center text-purple-800">
@@ -49,6 +51,7 @@ function AdminDataSummary({
               </div>
             </div>
           ) : categoriesSummary ? (
+            // categories summary section
             <div className="flex h-full justify-between w-full px-[20px]">
               <div className="flex h-full items-center justify-center py-[10px]">
                 <div className="bg-white px-[20px] h-full  rounded-[10px] flex items-center text-purple-800">
@@ -62,24 +65,77 @@ function AdminDataSummary({
                 </div>
               </div>
             </div>
+          ) : usersSummary ? (
+            // users summary section
+            <div className="flex h-full justify-between w-full px-[20px]">
+              <div className="flex h-full items-center justify-center py-[10px]">
+                <div className="bg-white px-[20px] h-full  rounded-[10px] flex items-center text-purple-800">
+                  <span className="text-[30px] text-center">
+                    Total <br /> Users
+                  </span>
+                  <span className="text-[30px] text-center ml-2"> : </span>
+                  <span className="ml-[10px] text-[50px] font-extrabold text-center">
+                    {usersSummary.totalUsers}
+                  </span>
+                </div>
+              </div>
+
+              <div className="flex h-full items-center justify-center py-[10px]">
+                <div className="bg-white px-[20px] h-full  rounded-[10px] flex items-center text-purple-800">
+                  <span className="text-[30px] text-center">
+                    Active <br /> Users
+                  </span>
+                  <span className="text-[30px] text-center ml-2"> : </span>
+                  <span className="ml-[10px] text-[50px] font-extrabold text-center">
+                    {usersSummary.totalActiveUsers}
+                  </span>
+                </div>
+              </div>
+
+              <div className="flex h-full items-center justify-center py-[10px]">
+                <div className="bg-white px-[20px] h-full  rounded-[10px] flex items-center text-purple-800">
+                  <span className="text-[30px] text-center">
+                    Banned <br /> Users
+                  </span>
+                  <span className="text-[30px] text-center ml-2"> : </span>
+                  <span className="ml-[10px] text-[50px] font-extrabold text-center">
+                    {usersSummary.totalBannedUsers}
+                  </span>
+                </div>
+              </div>
+
+              <div className="flex h-full items-center justify-center py-[10px]">
+                <div className="bg-white px-[20px] h-full  rounded-[10px] flex items-center text-purple-800">
+                  <span className="text-[30px] text-center">
+                    Verified <br /> Users
+                  </span>
+                  <span className="text-[30px] text-center ml-2"> : </span>
+                  <span className="ml-[10px] text-[50px] font-extrabold text-center">
+                    {usersSummary.totalEmailVerifiedUsers}
+                  </span>
+                </div>
+              </div>
+            </div>
           ) : (
             <div></div>
           )}
         </div>
 
         {/* button area */}
-        <div className="w-[130px] h-full rounded-[10px] ml-[20px] flex flex-col justify-center items-center">
-          {/* add new element button */}
-          <button
-            className="h-[80px] w-[80px] rounded-full  bg-purple-600 flex justify-center items-center hover:bg-purple-500 shadow-md"
-            onClick={onAddElementClick}
-          >
-            <FaPlus className="text-white text-[50px] transition-transform duration-300 ease-in-out hover:rotate-90" />
-          </button>
-          <span className="text-purple-600 text-center flex justify-center items-center text-[20px] font-bold">
-            Add New
-          </span>
-        </div>
+        {onAddElementClick && (
+          <div className="w-[130px] h-full rounded-[10px] ml-[20px] flex flex-col justify-center items-center">
+            {/* add new element button */}
+            <button
+              className="h-[80px] w-[80px] rounded-full  bg-purple-600 flex justify-center items-center hover:bg-purple-500 shadow-md"
+              onClick={onAddElementClick}
+            >
+              <FaPlus className="text-white text-[50px] transition-transform duration-300 ease-in-out hover:rotate-90" />
+            </button>
+            <span className="text-purple-600 text-center flex justify-center items-center text-[20px] font-bold">
+              Add New
+            </span>
+          </div>
+        )}
       </div>
     </div>
   );
